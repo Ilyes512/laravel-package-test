3f9c712052f9afdf81145c6f5428ed4d39ac2c71
Ilyes512 <ilyes.ahidar@gmail.com>
2025-02-21 23:38:56 +0100

    Initial commit

---
+++ tests/Feature/FoobarTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ilyes512\Foobar\Tests\Feature;
+
+class FoobarTest extends FeatureTestCase
+{
+    public function testFeatureExample(): void
+    {
+        self::markTestSkipped('This is just a placeholder feature test');
+    }
+}
---
+++ tests/Unit/FoobarTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ilyes512\Foobar\Tests\Unit;
+
+class FoobarTest extends UnitTestCase
+{
+    public function testUnitExample(): void
+    {
+        self::markTestSkipped('This is just a placeholder unit test');
+    }
+}
